c8714c9c74c91dd4adc3c6777fd4b5ec04b2e974
Nullp0 <danidelazzzari@gmail.com>
2024-12-14 15:40:50 +0100

    update per products

--- app/src/main/java/com/example/reuse/models/Product.java
+++ app/src/main/java/com/example/reuse/models/Product.java
@@ -1,5 +1,10 @@
 package com.example.reuse.models;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.io.Serializable;
 import java.util.List;
 
@@ -7,16 +12,77 @@ public class Product {
     private String idVenditore;
     private String nome;
     private String descrizione;
-    private Double prezzo;
+    private double prezzo;
     private boolean baratto;
+    private String imageUrl;
 
     public Product(){}
-    public Product(String idVenditore, String nome, String descrizione, double prezzo, boolean baratto){
+
+    //costruttore con tutti i dati
+    public Product(String idVenditore, String nome, String descrizione, double prezzo, boolean baratto, String imageUrl){
         this.idVenditore=idVenditore;
         this.nome=nome;
         this.descrizione=descrizione;
         this.prezzo=prezzo;
         this.baratto=baratto;
+        this.imageUrl=imageUrl;
+    }
+
+    //costruttore oggetto senza idvenditore per aggiunta nuovo oggetto
+    public Product(String nome, String descrizione, double prezzo, boolean baratto, String imageUrl){
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        this.idVenditore=currentUser.getUid();
+        this.nome=nome;
+        this.descrizione=descrizione;
+        this.prezzo=prezzo;
+        this.baratto=baratto;
+        this.imageUrl=imageUrl;
+    }
+
+    //costruttore oggetto a partire dall'ID
+    public Product(String pid){
+        DatabaseReference dbr = FirebaseDatabase.getInstance().getReference("Products").child(pid);
+        this.idVenditore=dbr.child("idVenditore").get().toString();
+        this.nome=dbr.child("nome").get().toString();
+        this.descrizione=dbr.child("descrizione").get().toString();
+        this.prezzo=Double.parseDouble(dbr.child("prezzo").get().toString());
+        this.baratto=Boolean.parseBoolean(dbr.child("baratto").get().toString());
+        this.imageUrl=dbr.child("imageUrl").get().toString();
+    }
+
+    //update dell'oggetto sul database
+    public void update(String pid, Product product){
+        updateIdVentirore(pid, product.idVenditore);
+        updateNome(pid,product.nome);
+        updateDescrizione(pid, product.descrizione);
+        updatePrezzo(pid, product.prezzo);
+        updateBaratto(pid, product.baratto);
+        updateImagineUrl(pid, product.imageUrl);
+    }
+
+    public void updateIdVentirore(String pid, String idVenditore){
+        DatabaseReference dbr = FirebaseDatabase.getInstance().getReference("Products").child(pid);
+        dbr.child("idVenditore").setValue(idVenditore);
+    }
+    public void updateNome(String pid, String nome){
+        DatabaseReference dbr = FirebaseDatabase.getInstance().getReference("Products").child(pid);
+        dbr.child("nome").setValue(nome);
+    }
+    public void updateDescrizione(String pid, String descrizione){
+        DatabaseReference dbr = FirebaseDatabase.getInstance().getReference("Products").child(pid);
+        dbr.child("descrizione").setValue(descrizione);
+    }
+    public void updatePrezzo(String pid, double prezzo){
+        DatabaseReference dbr = FirebaseDatabase.getInstance().getReference("Products").child(pid);
+        dbr.child("prezzo").setValue(prezzo);
+    }
+    public void updateBaratto(String pid, boolean baratto){
+        DatabaseReference dbr = FirebaseDatabase.getInstance().getReference("Products").child(pid);
+        dbr.child("idVenditore").setValue(baratto);
+    }
+    public void updateImagineUrl(String pid, String imageUrl){
+        DatabaseReference dbr = FirebaseDatabase.getInstance().getReference("Products").child(pid);
+        dbr.child("imageUrl").setValue(imageUrl);
     }
 
     public String getIdVenditore() {
