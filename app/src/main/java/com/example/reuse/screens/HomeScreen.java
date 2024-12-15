72375b7bed5a5c9210d5c0571fc51d2c9815ed62
MattewTop <matteo.pescarino@gmail.com>
2024-12-15 11:24:46 +0100

    Ensdrongo dongo gongo

--- app/src/main/java/com/example/reuse/screens/HomeScreen.java
+++ app/src/main/java/com/example/reuse/screens/HomeScreen.java
@@ -1,30 +1,48 @@
 package com.example.reuse.screens;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.renderscript.Sampler;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Range;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.reuse.R;
 import com.example.reuse.adapter.ProductAdapter;
 import com.example.reuse.models.Product;
+import com.google.android.material.slider.RangeSlider;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class HomeScreen extends Fragment implements ProductAdapter.OnItemClickListener  {
     private RecyclerView recyclerViewrecents;
     private RecyclerView recyclerViewlastAdded;
     private ProductAdapter adapter;
     private List<Product> productList;
+
     public HomeScreen() {
         // Required empty public constructor
     }
@@ -53,6 +71,172 @@ public class HomeScreen extends Fragment implements ProductAdapter.OnItemClickLi
         recyclerViewlastAdded.setAdapter(adapter);
         recyclerViewrecents.setAdapter(adapter);
 
+        //animazione pop up filtri
+        ImageButton menuButton = rootView.findViewById(R.id.menuButton);
+        LinearLayout filterContainer = rootView.findViewById(R.id.filterContainer);
+        filterContainer.post(new Runnable() {
+            @Override
+            public void run() {
+                float filterContainerHeight = filterContainer.getHeight();
+                filterContainer.setVisibility(View.GONE);
+                menuButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(filterContainer.getVisibility() == View.GONE){
+                            filterContainer.setVisibility(View.VISIBLE);
+                            filterContainer.setTranslationY(-filterContainerHeight);
+                            ObjectAnimator animation = ObjectAnimator.ofFloat(filterContainer,
+                                    "translationY",
+                                    -filterContainerHeight,
+                                    0);
+                            ValueAnimator va = ValueAnimator.ofInt(0, (int)filterContainerHeight);
+                            va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                                @Override
+                                public void onAnimationUpdate(@NonNull ValueAnimator animated) {
+                                    int value = (int) animated.getAnimatedValue();
+                                    ViewGroup.LayoutParams layoutParams = filterContainer.getLayoutParams();
+                                    layoutParams.height = value;
+                                    filterContainer.setLayoutParams(layoutParams);
+                                }
+                            });
+                            animation.setDuration(1000);
+                            va.setDuration(1000);
+                            animation.start();
+                            va.start();
+                        }
+                        else{
+                            ObjectAnimator animation = ObjectAnimator.ofFloat(filterContainer,
+                                    "translationY",
+                                    0,
+                                    -filterContainerHeight);
+                            ValueAnimator va = ValueAnimator.ofInt((int)filterContainerHeight, 0);
+                            va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                                @Override
+                                public void onAnimationUpdate(@NonNull ValueAnimator animated) {
+                                    int value = (int) animated.getAnimatedValue();
+                                    ViewGroup.LayoutParams layoutParams = filterContainer.getLayoutParams();
+                                    layoutParams.height = value;
+                                    filterContainer.setLayoutParams(layoutParams);
+                                }
+                            });
+                            animation.setDuration(1000);
+                            va.setDuration(1000);
+                            animation.start();
+                            va.start();
+                            animation.addListener(new AnimatorListenerAdapter() {
+                                @Override
+                                public void onAnimationEnd(Animator animation) {
+                                    super.onAnimationEnd(animation);
+                                    filterContainer.setVisibility(View.GONE);
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+        });
+
+        //animazione barButton e setting dell'attività del bottone (nella ricerca basterà controllare il parametro boolean barButtonActive
+        Button barButton = rootView.findViewById(R.id.barattoButton);
+        barButton.setBackgroundColor(Color.RED);
+        barButton.setOnClickListener(new View.OnClickListener() {
+            boolean barButtonActive = false;
+            @Override
+            public void onClick(View v) {
+                if(!barButtonActive){
+                    barButton.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.dark));
+                    barButtonActive = true;
+                }
+                else{
+                    barButton.setBackgroundColor(Color.RED);
+                    barButtonActive = false;
+                }
+            }
+        });
+
+        //barra del range di prezzo
+        EditText min = rootView.findViewById(R.id.minPriceSetter);
+        EditText max = rootView.findViewById(R.id.maxPriceSetter);
+        RangeSlider rangePrezzi = rootView.findViewById(R.id.rangePrezziSlider);
+
+        final boolean[] isUpdating = {false};
+
+        rangePrezzi.addOnChangeListener(new RangeSlider.OnChangeListener() {
+            @Override
+            public void onValueChange(@NonNull RangeSlider slider, float value, boolean fromUser) {
+                if(!isUpdating[0]){
+                    isUpdating[0] = true;
+                    float minVal = slider.getValues().get(0);
+                    float maxVal = slider.getValues().get(1);
+                    min.setText(String.valueOf((int) minVal));
+                    max.setText(String.valueOf((int) maxVal));
+                    isUpdating[0] = false;
+                }
+            }
+        });
+
+        min.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(!isUpdating[0]){
+                    String text = s.toString();
+                    if(!text.isEmpty()){
+                        try{
+                            isUpdating[0] = true;
+                            float minVal = Float.parseFloat(text);
+                            if(minVal>=rangePrezzi.getValueFrom()){
+                                if(minVal<=rangePrezzi.getValues().get(1)) rangePrezzi.setValues(minVal, rangePrezzi.getValues().get(1));
+                                else rangePrezzi.setValues(rangePrezzi.getValues().get(1), rangePrezzi.getValues().get(1));
+                            }
+                            else rangePrezzi.setValues(rangePrezzi.getValueFrom(), rangePrezzi.getValues().get(1));
+                        } catch (NumberFormatException e){
+                            e.printStackTrace();
+                        } finally {
+                            isUpdating[0] = false;
+                        }
+                    }
+                }
+            }
+        });
+
+        max.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(!isUpdating[0]){
+                    String text = s.toString();
+                    if(!text.isEmpty()){
+                        try{
+                            isUpdating[0] = true;
+                            float maxVal = Float.parseFloat(text);
+                            if(maxVal<=rangePrezzi.getValueTo()){
+                                if(maxVal>=rangePrezzi.getValues().get(0)) rangePrezzi.setValues(rangePrezzi.getValues().get(0), maxVal);
+                                else rangePrezzi.setValues(rangePrezzi.getValues().get(0), rangePrezzi.getValues().get(0));
+                            }
+                            else rangePrezzi.setValues(rangePrezzi.getValues().get(0), rangePrezzi.getValueTo());
+                        } catch (NumberFormatException e){
+                            e.printStackTrace();
+                        } finally {
+                            isUpdating[0] = false;
+                        }
+
+
+                    }
+                }
+            }
+        });
+
         return rootView;  // Return the view you inflated
     }
 
@@ -78,4 +262,5 @@ public class HomeScreen extends Fragment implements ProductAdapter.OnItemClickLi
         transaction.addToBackStack(null);  // Add to back stack if you want to go back to this fragment
         transaction.commit();
     }
+
 }
